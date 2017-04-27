b16e192382da10b9d22665fff26c85501a8c6c74
Spiros <spiroskleft@gmail.com>
2017-04-27 10:15:43 +0300

    Douleyei mia xara ! (oxi google sign in akoma kai thelei merikes allages sto UI)

--- app/src/main/java/com/example/spiros/savetheteacher/LoginWithGoogle.java
+++ app/src/main/java/com/example/spiros/savetheteacher/LoginWithGoogle.java
@@ -1,33 +1,33 @@
-package com.example.spiros.savetheteacher;
-
-/**
- * Created by Spiros on 25/4/2017.
- */
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class LoginWithGoogle extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_google);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-
-        if (fragment == null) {
-            fragment = new GPlusFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-
-
-        }
-
-
-    }
-}
+//package com.example.spiros.savetheteacher;
+//
+///**
+// * Created by Spiros on 25/4/2017.
+// */
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class LoginWithGoogle extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login_google);
+//
+//        FragmentManager fm = getSupportFragmentManager();
+//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+//
+//
+//        if (fragment == null) {
+//            fragment = new GPlusFragment();
+//            fm.beginTransaction()
+//                    .add(R.id.fragment_container, fragment)
+//                    .commit();
+//
+//
+//        }
+//
+//
+//    }
+//}
